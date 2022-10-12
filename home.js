53aaf7d801b46a4bd18523e9b08874629445a608
sabinatabbasum <110028827+sabinatabbasum@users.noreply.github.com>
2022-10-12 17:26:52 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,13 @@
+function loadCoupun(){
+    document.getElementById('coupon').style.visibility='visible';
+    document.getElementById('coup').style.opacity='0.7';
+}
+
+
+
+function closeCoupon(){
+
+
+    document.getElementById('coupon').style.visibility="hidden";
+    document.getElementById('coup').style.opacity='1;'
+}
